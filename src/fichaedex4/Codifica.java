d991eb81468d18fc48ef16c0ce26df5b924a68a6
2153749860 <tiagorocha1515@gmail.com>
2019-10-26 22:33:06 +0100

    Codifica completo

---
+++ src/fichaedex4/Codifica.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fichaedex4;
+
+/**
+ *
+ * @author tiago
+ */
+public class Codifica {
+    
+    public CircularArrayQueue<String> save;
+    public CircularArrayQueue<Integer> key;
+
+    public Codifica(String key) {
+        this.save = new CircularArrayQueue<>();
+        this.key = new CircularArrayQueue<>();
+        
+        passarKey(key);
+    }
+     
+     private void passarKey(String key){
+         char[] keyArray = key.toCharArray();
+         for (int i = 0; i < key.length(); i++) {
+             this.key.enqueue(Character.getNumericValue(keyArray[i]));
+         }
+     }
+     
+    public boolean codificar(String texto) throws EmptyCollectionException2{
+        if(texto == null){
+            return false;
+        } else{
+            char[] textoArray = texto.toCharArray();
+            
+            for(int i = 0; i <texto.length(); i++){
+                int k = key.dequeue();
+                
+                //passagem
+                int ch = (int)textoArray[i];
+                
+                ch = ch + k;
+                
+                char c;
+                c = (char) ch;
+                
+                String add = "" + c;
+                
+                this.save.enqueue(add);
+                
+            }
+            
+            return true;
+        }
+    }
+    
+    
+    
+    
+}
---
+++ src/fichaedex4/EmptyCollectionException2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fichaedex4;
+
+
+/**
+ *
+ * @author tiago
+ */
+public class EmptyCollectionException2 extends Exception{
+
+    public EmptyCollectionException2(String message) {
+        super(message);
+    }
+    
+}
